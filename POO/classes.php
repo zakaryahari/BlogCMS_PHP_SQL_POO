893c1356b1a6713171cbff86882e012e40dac7eb
zakarya hari <zakariahari42@gmail.com>
2025-12-24 11:20:12 +0100

    Creation de class user avec son attribute et methodes 'login','logout'.

---
+++ POO/classes.php
@@ -0,0 +1,21 @@
+<?php
+
+    class User {
+        protected int $id_utilisateur ;
+        protected String $username ;
+        protected String $email ;
+        protected String $password ;
+        protected DateTime $createdAt ;
+        protected DateTime $lastLogin ;
+        protected 
+        
+        public function login(string $email, string $password): bool {
+            foreach()
+            return false;
+        }
+
+        public function logout(): void {
+        }
+    }
+
+?>
